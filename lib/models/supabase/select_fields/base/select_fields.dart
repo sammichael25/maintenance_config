8f50a4b3f4e1d1dade1e7e60935c23e6756f691b
Michael Sam <michael.sam@rfhl.com>
2024-03-21 11:26:47 -0400

    Updated Select fields

--- lib/models/supabase/select_fields/base/select_fields.dart
+++ lib/models/supabase/select_fields/base/select_fields.dart
@@ -5,7 +5,7 @@ class SupabaseSelectConstants {
   static const String permissionsSelectFields = 'id, name, description, permission_group:group_id($permissionGroupSelectFields)';
   static const String roleSelectFields = 'id, name, department:department_id($departmentSelectFields), permissions($permissionsSelectFields)';
   static const String employeeSelectFields = 'id, employee_id, first_name, last_name, email, job_title, phone_extension, workstation_ip, role:role_id($roleSelectFields)';
-  static const String cveSelectFileds = 'id, number, url';
+  static const String cveSelectFileds = 'id, number';
   static const String portsSelectFileds = 'id, number, protocol';
   static const String vulnerabilitySelectFields = 'id, description, problem, details, severity, potential_resolution, analyst_resolution, cve_list:cve($cveSelectFileds), ports($portsSelectFileds)';
 }
