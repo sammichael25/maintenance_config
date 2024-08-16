bce26f1f2bd1420f6e024a1451bb0a510ccff161
Michael Sam <michael.sam@rfhl.com>
2024-08-16 16:37:35 -0400

    Updated Select fields

--- lib/models/supabase/select_fields/base/select_fields.dart
+++ lib/models/supabase/select_fields/base/select_fields.dart
@@ -2,7 +2,7 @@ class SupabaseSelectConstants {
   static const String officeSelectFields = 'id, office';
   static const String departmentSelectFields = 'id, name, office:office_id($officeSelectFields)';
   static const String permissionGroupSelectFields = 'id, name';
-  static const String permissionsSelectFields = 'id, name, description, permission_group:group_id($permissionGroupSelectFields)';
+  static const String permissionsSelectFields = 'id, name, description, permission_groups:group_id($permissionGroupSelectFields)';
   static const String roleSelectFields = 'id, name, department:department_id($departmentSelectFields), permissions($permissionsSelectFields)';
   static const String employeeSelectFields = 'id, employee_id, first_name, last_name, email, user_dn, job_title, phone_extension, workstation_ip, role:role_id($roleSelectFields)';
   static const String cveSelectFileds = 'id, number';
